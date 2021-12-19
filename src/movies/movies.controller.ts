bddf3b07f54ac7142b0c310332829422c7559eda
jessehj <jessehj@slogup.com>
2021-12-19 15:53:35 +0900

    feat: Try to make some simple apis with http request test

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,38 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Query,
+} from '@nestjs/common';
+
+@Controller('movies')
+export class MoviesController {
+  @Get()
+  getAll() {
+    return 'This will return all movies';
+  }
+
+  @Get('search')
+  search(@Query('year') year: string) {
+    return `We are searching for a movie made after: ${year}`;
+  }
+
+  @Get(':id')
+  getOne(@Param('id') id: string) {
+    return `This will return one movie with the id: ${id}`;
+  }
+
+  @Post()
+  create(@Body() movieData) {
+    console.log(movieData);
+    return 'This will create a movie';
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return `This will delete a movie: ${id}`;
+  }
+}
